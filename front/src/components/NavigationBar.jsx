9e07782ddec47329dbfbd66a440c1a638e60fd9c
vvnikita74 <78851101+Tupo-ok@users.noreply.github.com>
2023-05-21 21:51:00 +0300

    Lab 10-11

--- front/src/components/NavigationBar.jsx
+++ front/src/components/NavigationBar.jsx
@@ -1,42 +1,59 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate, Link } from 'react-router-dom';
 import BackendService from '../services/BackendService';
 import Utils from '../utils/Utils';
+import { userActions } from '../utils/Rdx';
+import { connect } from 'react-redux';
 import './NavbarStyle.css'
 
 class NavigationBarClass extends React.Component {
 
     constructor(props) {
         super(props);
+        this.logout = this.logout.bind(this)
+    }
+
+    
+    goHome() {
+        this.props.navigate('/home');
     }
 
     logout() {
         BackendService.logout().then(() => {
             Utils.removeUser();
-            this.goHome()
+            this.props.dispatch(userActions.logout());
+            this.props.navigate("/login");
         });
     }
 
     render() {
         let uname = Utils.getUserName();
         return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand className="ms-2 me-1"><FontAwesomeIcon icon={faHome} size='xs' />{' '}РПО</Navbar.Brand>
+            <Navbar bg="dark" expand="lg" variant="dark">
+                <button type="button" className="btn btn-outline-secondary mr-2" onClick={this.props.toggleSideBar}>
+                    <FontAwesomeIcon icon={faBars}/>
+                </button>
+                <Navbar.Brand className="ms-2 me-1">{' '}РПО</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/home">Главная</Nav.Link>
                     </Nav>
-                        <Navbar.Text className='me-2'>{uname}</Navbar.Text>
-                        { uname && <Nav.Link className="me-2" onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>}
-                        { !uname && <Nav.Link className="me-2" as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>}
+                    <Navbar.Text>{this.props.user && this.props.user.login}</Navbar.Text>
+                    { this.props.user && <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>}
+                    { !this.props.user && <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>}
                 </Navbar.Collapse>
             </Navbar>
         );
     }
+}
+
+const mapStateToProps = state => {
+    const { user } = state.authentication;
+    return { user };
 }
 
 const NavigationBar = props => {
@@ -44,4 +61,4 @@ const NavigationBar = props => {
     return <NavigationBarClass navigate={navigate} {...props} />
 }
 
-export default  NavigationBar;
+export default  connect(mapStateToProps)(NavigationBar);
--- front/src/services/BackendService.jsx
+++ front/src/services/BackendService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Utils from '../utils/Utils'
-
+import { store, alertActions } from '../utils/Rdx';
 
 const API_URL = 'http://localhost:8080/api'
 const AUTH_URL = 'http://localhost:8080/auth'
@@ -14,6 +14,57 @@ class BackendService {
     logout() {
         return axios.get(`${AUTH_URL}/logout`, {headers : {Authorization : Utils.getToken()}})
     }
+
+    retrieveAllCountries(countries) {
+        return axios.get('http://localhost:8080/api/countries', countries, {headers : {Authorization : Utils.getToken()}});
+    }
+    
+    retrieveCountry(id) {
+        return axios.get(`${API_URL}/countries/${id}`, {headers : {Authorization : Utils.getToken()}});
+    }
+    
+    createCountry(country) {
+        return axios.post(`${API_URL}/countries`, country, {headers : {Authorization : Utils.getToken()}});
+    }
+    
+    updateCountry(country) {
+        return axios.put(`${API_URL}/countries/${country.id}`, country, {headers : {Authorization : Utils.getToken()}});
+    }
+    
+    deleteCountries(countries) {
+        return axios.post(`${API_URL}/deletecountries`, countries, {headers : {Authorization : Utils.getToken()}});
+    }
+
 }
+
+function showError(msg)
+{
+    store.dispatch(alertActions.error(msg))
+}
+
+axios.interceptors.request.use(
+    config => {
+        store.dispatch(alertActions.clear())
+        let token = Utils.getToken();
+        if (token)
+            config.headers.Authorization = token;
+        return config;
+    },
+    error => {
+        showError(error.message)
+        return Promise.reject(error);
+    })
+
+axios.interceptors.response.use(undefined,
+    error => {
+        if (error.response && error.response.status && [401, 403].indexOf(error.response.status) !== -1)
+            showError("Ошибка авторизации")
+        else if (error.response && error.response.data && error.response.data.message)
+            showError(error.response.data.message)
+        else
+            showError(error.message)
+        return Promise.reject(error);
+    })
+        
 
 export default new BackendService()
